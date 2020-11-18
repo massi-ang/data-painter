27f3182c79ae3d5b9bb6f1c67f63cf32a15a4112
Massimiliano Angelino <angmas@amazon.com>
2020-11-18 16:23:26 +0100

    last iteration

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,33 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <b-navbar>
+        <template slot="brand">
+            <b-navbar-item tag="router-link" :to="{ path: '/' }">
+                <img
+                    src="https://raw.githubusercontent.com/buefy/buefy/dev/static/img/buefy-logo.png"
+                    alt="Lightweight UI components for Vue.js based on Bulma"
+                >
+            </b-navbar-item>
+        </template>
+        <template slot="start">
+            <b-navbar-item href="#">
+                Home
+            </b-navbar-item>
+            <b-navbar-item href="#">
+                Documentation
+            </b-navbar-item>
+            <b-navbar-dropdown label="Info">
+                <b-navbar-item href="#">
+                    About
+                </b-navbar-item>
+                <b-navbar-item href="#">
+                    Contact
+                </b-navbar-item>
+            </b-navbar-dropdown>
+        </template>
+
+       
+    </b-navbar>
     <router-view/>
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,43 +1,64 @@
 <template>
-  <div class="home">
-    <button @click="sendClear()">Clear</button>
-    <SeriePainter v-on:done="save" :series="paintSeries" v-on:saveEvents="saveEvents"/>
-    <div v-for="(s, i) in series" :key="s.name">
-      <input  type="checkbox" :id="i" @change="handleChange()" v-model="s.selected">
-      <label :for="s.name">{{s.name}}</label>
-      <span :style="{backgroundColor: s.color, color: s.color}">___</span>
-      <button @click="deleteSerie(i)">Delete</button>
-      <button @click="exportSerie(i)">Export</button>
+  <div class="section">
+    <div class="level">
+      <b-button @click="sendClear()">Clear</b-button>
+      <b-button type="is-primary" @click="emitEventRec()">{{eventRecord}}EventRec</b-button>
     </div>
-    <div v-for="(s, i) in events" :key="s.name">
-      <input  type="checkbox" :id="i" @change="handleChange()" v-model="s.selected">
-      <label :for="s.name">{{s.name}}</label>
-      <span :style="{backgroundColor: s.color, color: s.color}">___</span>
-      <button @click="deleteEvents(i)">Delete</button>
-      <button @click="exportEvents(i)">Export</button>
+    <div class="level">
+      <SeriePainter v-on:done="save" :series="paintSeries" v-on:saveEvents="saveEvents" />
     </div>
-  <div> 
-    <button @click="emitEventRec()">{{eventRecord}}EventRec</button>
-  </div>
-    <div>
-      <button @click="rebase()">Rebase at Zero</button>
-      <button @click="highCap()">Cap upper</button>
-      <button @click="lowCap()">Cap lower</button>
-       <button @click="smooth()">Smooth</button>
-      <div>
-        <label for="base">Base</label>
-      <input id="base" type="text" v-model="base"/>
-      <label for="low">Low</label>
-      <input id="low" type="text" v-model="low"/>
-      <label for="high">High</label>
-      <input id="high" type="text" v-model="high"/>
-      <label for="win">Window</label>
-      <input id="win" type="text" v-model="window"/>
-</div>
+    <div class="columns">
+      <div class="column">
+    <section>
+      <div class="block" v-for="(s, i) in series" :key="s.name">
+        <b-checkbox :id="i" @change="handleChange()" v-model="s.selected">{{s.name}}<span :style="{backgroundColor: s.color, color: s.color}">___</span></b-checkbox>
+        
+        <b-button outlined type="is-primary" @click="deleteSerie(i)">Delete</b-button>
+        <b-button @click="exportSerie(i)">Export</b-button>
       </div>
-  <div>
-    {{exportedSerie}}
-</div>
+      <div v-for="(s, i) in events" :key="s.name">
+        <b-checkbox :id="i" @change="handleChange()" v-model="s.selected" />
+        <label :for="s.name">{{s.name}}</label>
+        <span :style="{backgroundColor: s.color, color: s.color}">___</span>
+        <b-button @click="deleteEvents(i)">Delete</b-button>
+        <b-button @click="exportEvents(i)">Export</b-button>
+      </div>
+    </section>
+      </div>
+      <div class="column">
+
+    <section class="section">
+     <div class="buttons">
+        <b-button type="is-info" @click="rebase()">Rebase at Zero</b-button>
+        <b-button type="is-info" @click="highCap()">Cap upper</b-button>
+        <b-button type="is-info" @click="lowCap()">Cap lower</b-button>
+        <b-button type="is-info" @click="smooth()">Smooth</b-button>
+        <b-button type="is-info" @click="average()">Average</b-button>
+        <b-button type="is-info" @click="sample()">Sample</b-button>
+     </div>
+    </section>
+      <section>
+        <div class="block">
+        <b-field label="Base" label-position="on-border">
+          <b-input id="base" type="text" v-model="base" />
+        </b-field>
+        <b-field label="Low" label-position="on-border">
+          <b-input id="low" type="text" v-model="low" />
+        </b-field>
+        <b-field label="High" label-position="on-border">
+          <b-input id="high" type="text" v-model="high" />
+        </b-field>
+        <b-field label="Window" label-position="on-border">
+        <b-input id="win" type="text" v-model="window" />
+        </b-field>
+        </div>
+      </section>
+   
+    </div>
+    </div>
+    <section>
+      <div>{{exportedSerie}}</div>
+    </section>
   </div>
 </template>
 
@@ -51,93 +72,125 @@ export default {
   components: {
     SeriePainter
   },
-  data() {
+  data () {
     return {
       series: [],
       events: [],
       exportedSerie: '',
       paintSeries: [],
-      low:-100,
+      low: -100,
       high: 100,
       base: 0,
       window: 5,
-      eventRecord: 'start'
+      eventRecord: 'start',
     }
   },
   methods: {
-    save(serie) {
-      this.series.push({name: `S-${serie.color}`, ...serie, selected: true})
+    save (serie) {
+      this.series.push({ name: `S-${serie.color}`, ...serie, selected: true })
     },
-    saveEvents(events) {
+    saveEvents (events) {
       console.log("save")
-      this.events.push({name: `E-${Math.random()*99999}`, ...events, selected:true})
+      this.events.push({ name: `E-${Math.random() * 99999}`, ...events, selected: true })
     },
-    deleteSerie(i) {
-      this.series = this.series.filter((v,idx)=>{if (idx!=i) {return true} else {return false}})
+    deleteSerie (i) {
+      this.series = this.series.filter((v, idx) => { if (idx != i) { return true } else { return false } })
     },
-    exportSerie(i) {
+    exportSerie (i) {
       console.log(this.series[i])
-      
+
       this.exportedSerie = this.series[i].serie
       navigator.clipboard.writeText(JSON.stringify(this.exportedSerie))
     },
-    deleteEvents(i) {
-      this.events = this.events.filter((v,idx)=>{if (idx!=i) {return true} else {return false}})
+    deleteEvents (i) {
+      this.events = this.events.filter((v, idx) => { if (idx != i) { return true } else { return false } })
     },
-    exportEvents(i) {
+    exportEvents (i) {
       console.log(this.events[i])
       var toExport = {}
       this.events[i].data.forEach((x) => toExport[x.toString()] = 'EVENT')
       this.exportedSerie = toExport
       navigator.clipboard.writeText(JSON.stringify(this.exportedSerie))
     },
-    sendClear() {
+    sendClear () {
       EventBus.$emit('clear', {})
     },
-    handleChange() {
-      this.paintSeries = this.series.filter(s => s.selected) 
+    handleChange () {
+      this.paintSeries = this.series.filter(s => s.selected)
     },
-    rebase() {
+    rebase () {
       this.series.filter(s => s.selected).map(s => rebase(s, parseInt(this.base)))
       this.handleChange()
     },
-    highCap() {
+    highCap () {
       this.series.filter(s => s.selected).map(s => highCap(s, parseInt(this.high)))
       this.handleChange()
     },
-    lowCap() {
+    lowCap () {
       this.series.filter(s => s.selected).map(s => lowCap(s, parseInt(this.low)))
       this.handleChange()
     },
-    smooth() {
+    smooth () {
       this.series.filter(s => s.selected).map(s => smooth(s, parseInt(this.window)))
       this.handleChange()
     },
-    emitEventRec() {
-      EventBus.$emit(this.eventRecord+'EventRecord', {})
-      this.eventRecord = (this.eventRecord == 'start' ? 'stop': 'start')
+    sample () {
+      this.series.filter(s => s.selected).map(s => sample(s, parseInt(this.window)))
+      this.handleChange()
+    },
+    average () {
+      this.series.filter(s => s.selected).map(s => average(s, parseInt(this.window)))
+      this.handleChange()
+    },
+    emitEventRec () {
+      EventBus.$emit(this.eventRecord + 'EventRecord', {})
+      this.eventRecord = (this.eventRecord == 'start' ? 'stop' : 'start')
     }
   }
 }
 
-function rebase(s, base) {
-  var min = s.serie.reduce((x, y) => x <y ? x: y)
+function rebase (s, base) {
+  var min = s.serie.reduce((x, y) => x < y ? x : y)
   s.serie = s.serie.map(v => v - min + base)
-  
+
 }
 
-function lowCap(s, low) {
-  s.serie = s.serie.map( v=> ( v < low ? low : v))
+function lowCap (s, low) {
+  s.serie = s.serie.map(v => (v < low ? low : v))
 }
 
-function highCap(s, high) {
-  s.serie = s.serie.map( v=> ( v > high ? high : v))
+function highCap (s, high) {
+  s.serie = s.serie.map(v => (v > high ? high : v))
 }
 
-function smooth(s,w) {
-  var a = s.serie.slice(0,w)
-  s.serie = s.serie.map( v=> {a.shift(); a.push(v); return a.reduce((x,y)=>x+y)/a.length})
+function smooth (s, w) {
+  var a = s.serie.slice(0, w)
+  s.serie = s.serie.map(v => { a.shift(); a.push(v); return a.reduce((x, y) => x + y) / a.length })
+}
+
+
+function sample (s, w) {
+  s.serie = s.serie.filter((v, i) => i % w === 0)
+}
+
+
+function average (s, w) {
+  var i = 0
+  var new_s = []
+  while (i < s.serie.length) {
+    let a = s.serie.slice(i, i + w)
+    new_s.push(a.reduce((x, y) => x + y) / w)
+    i += w
+  }
+
+  s.serie = new_s
 }
 
 
 </script>
+
+<style scoped>
+#input {
+  width: 500px;
+}
+</style>
